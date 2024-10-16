d096e708b3be2fb73464d5c0669e5d9eb5a4c767
eoin ager <G00431577@atu.ie>
2024-10-16 18:44:59 +0100

    Students Input name email and course

--- src/main/java/ie/atu/week3/Student_App.java
+++ src/main/java/ie/atu/week3/Student_App.java
@@ -1,4 +1,34 @@
 package ie.atu.week3;
 
+import java.util.Scanner;
+
 public class Student_App {
+    public static void main(String[] args) {
+
+        //Student 1
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the name of the student: ");
+        String name = sc.nextLine();
+        System.out.println("Please enter the email of the student: ");
+        String email = sc.nextLine();
+        System.out.println("Please enter the course of the student: ");
+        String course = sc.nextLine();
+
+        //Student 2
+        System.out.println("Please enter the name of the student: ");
+        String name2 = sc.nextLine();
+        System.out.println("Please enter the email of the student: ");
+        String email2 = sc.nextLine();
+        System.out.println("Please enter the course of the student: ");
+        String course2 = sc.nextLine();
+
+        //student 3
+        System.out.println("Please enter the name of the student: ");
+        String name3 = sc.nextLine();
+        System.out.println("Please enter the email of the student: ");
+        String email3 = sc.nextLine();
+        System.out.println("Please enter the course of the student: ");
+        String course3 = sc.nextLine();
+
+
 }
